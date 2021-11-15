88aab38dd4749b66d1f7790c3df3c2dbf8561791
Robert McDermott <robert.c.mcdermott@gmail.com>
2021-11-15 12:49:40 -0800

    initial commit

---
+++ certificate_inventory.py
@@ -0,0 +1,109 @@
+#!/usr/bin/python3
+"""
+Short Description:  SSL/TLS Certificate Inventory Script
+Long Description:   A script that interrogates the provided CIDR network ranges, finds HTTPS web servers
+                    running on the desired port(s) and gathers provides a report to standard output in
+                    CSV format, containing the following columns:
+
+                      - IP Address
+                      - DNS hostname of the IP address (reverse lookup)
+                      - Subject (hostname) of the certificate
+                      - The issuer of the certificate
+                      - The start date of the certificate
+                      - The expiration data of the certificate
+                      - The unique fingerprint of the certificate
+                    
+                    This is a quick hackish was to do this and needs a lot of improvements
+"""
+import configparser
+import ipaddress
+import os
+import random
+import socket
+import sys
+import ssl
+import urllib.request
+
+def main():
+    print("IP:port, DNS Hostname, Certificate Name, Issuer, Start Date, Expiration Date, Fingerprint")
+    ips = allips(subnets)
+    random.shuffle(ips)
+    for ip in ips:
+        for port in ports:
+            if isalive(ip, port):
+                print(getcertinfo(ip, port))
+                sys.stdout.flush()
+
+def getcertinfo(ip, port):
+    """Takes an IP and port number and returns information about the certificate and host. The 
+    parsing of the fields is an attempt to just get the basic information as some certificates
+    have some very verbose infomation that is not useful for a report of this type. This hackish
+    parsing should be replaced with some more elegant regular expressions.
+    """
+    out = os.popen(cmd % (ip, port)).readlines()
+    out = [x.replace(" =", "=") for x in out]
+    cert = out[0].split("CN=")[-1].split("/")[0].split(',')[0].strip().lower()
+    issuer = out[1].split("CN=")[-1].split(",")[0].split("/")[0].strip()
+    expire = out[2].split("=")[-1].strip()
+    start = out[3].split("=")[-1].strip()
+    fingerprint = out[4].split("=")[-1].strip()
+    hostname = dnsname(ip)
+    return "%s, %s, %s, %s, %s, %s, %s" % (ip, hostname, cert, issuer, start, expire, fingerprint)
+
+def isalive(ip, port):
+    """Takes and IP address and port and returns True if an HTTPS web server is located at that
+    IP/port and False if not.
+    """
+    ctx = ssl.create_default_context()
+    ctx.check_hostname = False
+    ctx.verify_mode = ssl.CERT_NONE
+    host = "https://%s:%s" % (ip, port) 
+    try:
+        socket.setdefaulttimeout(1)
+        code = urllib.request.urlopen(host, context=ctx).getcode()
+        return True
+    except urllib.error.URLError:
+        return False
+    except:
+        return False 
+
+def cidr2ip(cidr):
+    """Take a network CIDR range and return a list of IP addresses"""
+    try:
+        return [str(ip) for ip in ipaddress.IPv4Network(cidr)]
+    except:
+        print("Error: Invalid CIDR range defined\n")
+        sys.exit(1)
+
+
+def allips(subnets):
+    """Takes a CIDR formated (192.160.1.0/24) subnet and returns a list of all the individual
+    IP addresses that the provided CIDR contains. /32 is as single IP address CIDR.
+    """
+    ips = []
+    for subnet in subnets:
+        ips.extend(cidr2ip(subnet))
+    return ips
+
+
+def dnsname(ip):
+    """Returns the DNS hostname of the provided IP address if it's in DNS (PTR record)"""
+    try:
+        return socket.gethostbyaddr(ip)[0]
+    except:
+        return 'null' 
+
+if __name__ == "__main__":
+    cmd ="echo -n| openssl s_client -connect %s:%s 2>/dev/null | openssl x509 -noout -subject -issuer -enddate -startdate -fingerprint"
+    #subnets = ['140.107.42.0/23','140.107.88.0/23','140.107.134.0/23','140.107.116.0/23','140.107.80.0/24','140.107.52.0/23',140.107.170.0/23, 140.107.50.0/23]
+
+    configfile = 'config.ini'
+    if not os.path.exists(configfile):
+        print("Error: configuration file '%s' missing\n" % configfile)
+        sys.exit(1)
+    config = configparser.ConfigParser()
+    config.read(configfile)
+    subnets = [x.strip() for x in config.get('config', 'subnets').split()]
+    ports = [x.strip() for x in config.get('config', 'ports').split()]
+
+    main()
---
+++ readme.md
@@ -0,0 +1,39 @@
+# SSL/TLS Certificate Inventory 
+
+## Description 
+
+A script that interrogates the provided CIDR network ranges, finds HTTPS web servers running on the desired port(s) and gathers provides a report to standard output in CSV format, containing the following columns:
+- IP Address
+- DNS hostname of the IP address (reverse lookup)
+- Subject (hostname) of the certificate
+- The issuer of the certificate
+- The start date of the certificate
+- The expiration data of the certificate
+- The unique fingerprint of the certificate   
+- This is a quick hackish was to do this and needs a lot of improvements
+
+## Requirements
+
+- Linux operating system (tested on Ubuntu 18.04)
+- Openssl command-line utility (sudo apt install openssl)
+- Python 3.x
+
+
+## Usage
+
+Edit the configuration file "config.ini" and define the subnets that you want to interrogate in valid CIDR format and the ports that you want check for HTTPS web servers. Each subnet and port has to be on its own line and indented in the correct sections as shown below:
+
+```ini
+[config]
+subnets = 
+    192.168.0.0/24
+    172.17.64.0/28
+ports =
+    443
+```
+
+After the configuration is in place, run the following command from a host that has access to the configured networks. In this example the report will be located in the file 'cert-inventory-2021-11-15.csv'.
+
+```bash
+python3 certificate_inventory.py > cert-inventory-2021-11-15.csv
+```
